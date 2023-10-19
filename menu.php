1bff410ea72b82cb0dac2fd73178a6e6d3aafc38
TaelanTout <taelan668@gmail.com>
2023-10-19 11:53:19 +0200

    fifth commit

--- menu.php
+++ menu.php
@@ -125,7 +125,7 @@
   <div class="progress-bar" role="progressbar" style="width: 5%;" aria-valuenow="5" aria-valuemin="0" aria-valuemax="100">Et 5% de CSS</div>
 </div>
 <hr>
-<h4>*HACK étant un fichier PHP contenat uniquement du HTML</h4>
+<h4>*HACK étant un fichier PHP contenant uniquement du HTML</h4>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html
